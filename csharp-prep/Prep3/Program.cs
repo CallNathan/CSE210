494fd1a035d7546f99a8dc452bff4f309f014441
CallNathan <n8.t.call@gmail.com>
2024-04-29 08:26:26 -0600

    prep3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        //string Snumber = "";
+        string SGuess = "";
+        int Guess = 0;
+        //Console.Write("What is the magic number?");
+        //Snumber = Console.ReadLine();
+        //int number = int.Parse(Snumber);
+
+        Random randomnumber = new Random();
+        int number = randomnumber.Next(1,1000);
+
+        while (number != Guess) {
+            Console.WriteLine("What is your guess?");
+            SGuess = Console.ReadLine();
+            Guess = int.Parse(SGuess);
+
+            if (Guess > number) {
+                Console.WriteLine("Lower");
+            }
+            else if (Guess < number) {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("You Guessed it!");
+            }
+        }
+        
     }
 }
