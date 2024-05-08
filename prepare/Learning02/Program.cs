22866509cfd18f004b73620939b6575d81db5e54
CallNathan <n8.t.call@gmail.com>
2024-05-08 09:29:58 -0600

    individual teamwork work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+            job1._company = "Microsoft";
+            job1._jobTitle = "Software Engineer";
+            job1._startYear = 2019;
+            job1._endYear = 2022;     
+
+        Console.WriteLine(job1);    
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
+        string randomPrompt = "";
         Console.WriteLine("Hello Sandbox World!");
+        randomPrompt = promptGenerator();
+        List<string> entry = CreateEntry(randomPrompt);
+        
     }
+    //random output generator from list, should work well enough for a list of any length that isnt 0
+    static string promptGenerator() 
+    {
+	    List<string> promptList = new List<string> //you can just declare a list directly if you have values you want to put in it beforehand
+        {
+        "Who was the most interesting person I interacted with today?",
+	    "What was the best part of my day?",
+	    "How did I see the hand of the Lord in my life today?",
+	    "What was the strongest emotion I felt today?",
+	    "If I had one thing I could do over today, what would it be?"
+        };
+
+	    Random rand = new();
+	    int randomNumber = rand.Next(0, promptList.Count);
+	    string randomPrompt = promptList[randomNumber];
+
+	    //Console.WriteLine(randomPrompt);
+        return randomPrompt;
+    }
+    static List<string> CreateEntry(string Prompt) 
+    {
+        List<string> entry = new();
+        Console.WriteLine("What is todays date?");
+        string date = Console.ReadLine();
+        Console.WriteLine($"please write a response to the prompt {Prompt}");
+        string response = Console.ReadLine();
+        entry.Add(date);
+        entry.Add(Prompt);
+        entry.Add(response);
+
+        //To print each line in a list of strings
+
+        // foreach (string Item in entry) {
+        //     Console.WriteLine(Item);
+        // }
+        
+
+        return entry;
+    }
+
 }
