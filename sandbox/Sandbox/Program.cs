4bdbf80df00a429431d378f28e6184e98a1d0ab5
CallNathan <n8.t.call@gmail.com>
2026-03-02 09:41:14 -0700

    extra class practice in sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,35 @@
 using System;
-
+using System.Security.Cryptography.X509Certificates;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("I put this here to test GIT pushes");
+        int x = 20;
+        Console.WriteLine($"In main: {x}");
+        passByValue(x);
+        Console.WriteLine($"in main: {x}");
+        passByRef(ref x);
+        Console.WriteLine($"in main: {x}");
+
+        int z;
+        passByOut(out z);
+        Console.WriteLine(z);
+
+    }
+
+    public static void passByRef(ref int x)
+    {
+        x++;
+        Console.WriteLine($"in value function: {x}");
+    }
+    public static void passByValue(int x)
+    {
+        x++;
+        Console.WriteLine($"In value: {x}");
+    }
+    public static void passByOut(out int y)
+    {
+        y = 10;
     }
 }
+
