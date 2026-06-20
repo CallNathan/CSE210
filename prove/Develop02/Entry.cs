6bb52f82cca4a67e350cca4458675eb9c5183e6a
CallNathan <n8.t.call@gmail.com>
2026-06-20 14:09:14 -0600

    Fetch random prompt from journal and test it

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,22 @@
+public class Entry
+{
+    public Random random = new Random();
+    public string _date;
+    public string _prompt;
+    public int _response;
+    public int _thoughts;
+    
+    public void Load(){
+
+        //System.IO.File.ReadAllLines();
+    }
+    public void GetPrompt() {
+        
+        int loop = 0;
+        while (loop != 10) {
+            int randomIndex = random.Next(0, Journal._prompts.Count);
+            Console.WriteLine(randomIndex);
+            loop = loop + 1;
+        }
+    }
+};
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,14 @@
+public class Journal()
+{
+    public List<Entry> _entries;
+    public static List<string> _prompts = [
+        "Who was the most interesting person I met today?",
+        "What was the best part of my day?",
+        "How did i see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What did I find easy to do today?",
+        "Was I kind to someone today? How was I kind?",
+        "What do I plan to do tomorrow?",
+    ]; //I got static from this source since i needed a static list apperently. https://stackoverflow.com/questions/19393481/c-sharp-initialize-a-static-list-in-a-class
+}
